867b89952dfd9b141b5c4a5821d8e23846d29393
Fatima Garcia <liss260120@gmail.com>
2023-05-12 11:36:18 -0600

    ordenando un arreglo en paralelo

--- ColecDictionary/Program.cs
+++ ColecDictionary/Program.cs
@@ -1,12 +1,17 @@
-﻿
+﻿//programa que permita visualizar los departamentos de nicaragua con
+//su cantidad poblacional.
+//Encuentre: Mayor, menor, sumas y ordene los datos
+
 using System.Collections;
 String[] Departamento={ "Boaco","Carazo","Chinandega","Chontales","Costa Caribe Norte","Costa Caribe Sur","Estelí","Granada","Jinotega","León","Madriz","Managua",    "Masaya",    "Matagalpa",    "Nueva Segovia",    "Río San Juan",    "Rivas"};
 int[] Poblacion={185013,197139,439906,190863,530586,414543,229866,214317,475630,421050,174744,1546939,391903,593503,271581,135446,182645};
 
 int maxDep=Poblacion.Max();
+//Encontrar el mayor y el menor
 int minDep =Poblacion.Min();
 string maxDepkey = "", minDepkey = "";
 
+//Encontrar los nombres del mayor y el menor
 for (var i = 0; i < Poblacion.Length; i++)
 {
     if(Poblacion[i]==maxDep)
