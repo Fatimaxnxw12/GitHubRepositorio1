844be151d38f7e02e923f0ee5d13bbf04181b3b4
Fatima Garcia <liss260120@gmail.com>
2023-05-28 17:47:19 -0600

    ya no hay conflicto

--- ColecDictionary/Program.cs
+++ ColecDictionary/Program.cs
@@ -1,42 +1,32 @@
-﻿using System.Collections;
-//definicion e inicializacion de los arreglos en paralelo y el diccionario
-String[] Departamento={ "Boaco","Carazo","Chinandega","Chontales","Costa Caribe Norte","Costa Caribe Sur","Estelí","Granada","Jinotega","León","Madriz","Managua",    "Masaya",    "Matagalpa",    "Nueva Segovia",    "Río San Juan",    "Rivas"};
-int[] Poblacion={185013,197139,439906,190863,530586,414543,229866,214317,475630,421050,174744,1,575,819,391903,593503,271581,135446,182645};
-<<<<<<< HEAD
-
-int maxDep=Poblacion.Max();
+﻿//programa que permita visualizar los departamentos de nicaragua con
+//su cantidad poblacional.
+//Encuentre: Mayor, menor, sumas y ordene los datos
+using System.Collections;
+String[] Departamento = { "Boaco", "Carazo", "Chinandega", "Chontales", "Costa Caribe Norte", "Costa Caribe Sur", "Estelí", "Granada", "Jinotega", "León", "Madriz", "Managua", "Masaya", "Matagalpa", "Nueva Segovia", "Río San Juan", "Rivas" };
+int[] Poblacion = { 185013, 197139, 439906, 190863, 530586, 414543, 229866, 214317, 475630, 421050, 174744, 1546939, 391903, 593503, 271581, 135446, 182645 };
+Dictionary<string, int> diccionario = Departamento
+          .Zip(Poblacion, (k, v) => new { Clave = k, Valor = v })
+          .ToDictionary(x => x.Clave, x => x.Valor);
 //Encontrar el mayor y el menor
-int minDep =Poblacion.Min();
+int maxDep = Poblacion.Max();
+int minDep = Poblacion.Min();
 string maxDepkey = "", minDepkey = "";
-
 //Encontrar los nombres del mayor y el menor
 for (var i = 0; i < Poblacion.Length; i++)
 {
-    if(Poblacion[i]==maxDep)
-        maxDepkey=Departamento[i];
-    if(Poblacion[i]==minDep)
-    minDepkey = Departamento[i];
-    Console.WriteLine($"mayor poblacion:{maxDepkey}");
-    Console.WriteLine($"menor poblacion:{minDepkey}");
+    if (Poblacion[i] == maxDep)
+        maxDepkey = Departamento[i];
+    if (Poblacion[i] == minDep)
+        minDepkey = Departamento[i];
 }
-
-=======
-Dictionary<string, int> diccionario = Departamento
-      .Zip(Poblacion, (k, v)=> new {Clave = k, Valor = v })
-      .ToDictionary(x=> x.Clave,x=>x.Valor);
-//ordenando los nombres de menor a mayor
-var ordenado = diccionario.OrderBy(x => x.Value).ToDictionary(x => x.Key,x => x.Value);
-//fijando los nombres de los departamentos con menor y mayor poblacion
-string minDepkey = ordenado.First().Key;
-string maxDepkey = ordenado.Last().Key;
-//reasignacion de los arreglos en paralelo
+//Ordenando con diccionario y Orderby de LINQ
+var ordenado = diccionario.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 Departamento = ordenado.Keys.ToArray();
 Poblacion = ordenado.Values.ToArray();
-//mostrar los arreglos ordenados de menor a mayor.
->>>>>>> versionSimple
+//mostrar los resultados
 for (var i = 0; i < Poblacion.Length; i++)
-  Console.WriteLine($"{Departamento[i],20} ==> {Poblacion[i],10:N0}");
-//suma de toda la poblacion y nombre de mayor a menor
-Console.WriteLine($"Departamento con mayor poblacion:{maxDepkey}");
-Console.WriteLine($"Departamento con menor poblacion:{minDepkey}");
+    Console.WriteLine($"{Departamento[i],20} ==> {Poblacion[i],10:N0}");
+//Sumar todas las poblaciones con LINQ
+Console.WriteLine($"Población General:{Poblacion.Sum():N0}");
+Console.WriteLine($"Departamento con mayor Población:{maxDepkey}");
 Console.WriteLine($"Departamento con menor Población:{minDepkey}");
